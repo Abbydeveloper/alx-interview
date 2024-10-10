fc7027486ae33e6d7b33e75a76441f5cd0a0dcd7
Adeboga Abigail <abbyboga@gmail.com>
2024-10-10 04:50:33 -0700

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+def canUnlockAll(boxes):
+    """ 
+    Args: boxes - the number of locked boxes
+
+    Return: True if all boxes can be opened, else return False
+    """
+    n = len(boxes)
+    unlocked_boxes = []
+
+    for i in range(0, n - 1):
+        for key in boxes[i]:
+            if key not in unlocked_boxes and key < n:
+                unlocked_boxes.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False

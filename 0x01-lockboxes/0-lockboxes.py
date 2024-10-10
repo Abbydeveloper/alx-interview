d677b563c82a6a04690045d1ac7f985bdfe65793
Adeboga Abigail <abbyboga@gmail.com>
2024-10-10 07:16:51 -0700

    Fix?

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,7 +15,7 @@ def canUnlockAll(boxes):
     n = len(boxes)
     unlocked_boxes = [0]
 
-    for i in unlocked_boxes:
+    for i in range(n):
         for key in boxes[i]:
             if key not in unlocked_boxes and key < n:
                 unlocked_boxes.append(key)

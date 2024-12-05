0373d82a6583f0d4ee6dac9059d30a6183221bd9
Adeboga Abigail <abbyboga@gmail.com>
2024-12-05 21:02:58 +0000

    Rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,10 +12,10 @@ def rotate_2d_matrix(matrix):
     """
     n = len(matrix)
 
-    for i in range(n / 2):
+    for i in range(n // 2):
         for j in range(n):
-            matrix[i][j], matrix[n - i - 1][j] = matrix[n - i - 1][j],
-            matrix[i][j]
+            matrix[i][j], matrix[n - i - 1][j] = \
+            matrix[n - i - 1][j], matrix[i][j]
 
     for i in range(n):
         for j in range(i + 1, n):

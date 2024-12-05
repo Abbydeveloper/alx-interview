ea23db3aa4077b85e4ecd448f6097fa0f800d4f1
Adeboga Abigail <abbyboga@gmail.com>
2024-12-05 13:17:59 +0000

    Calculate Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,21 +12,20 @@ def island_perimeter(grid):
         Perimeter of the island
     """
 
-    perimeter = 0;
+    perimeter = 0
 
     for i in range(len(grid)):
         length = 0
         for j in range(len(grid)):
-            if grid[i][j] == 0:
-                continue
-            if i == 0 or grid[i - 1][j] == 0:
-                length += 1
-            if j == 0 or grid[i][j - 1] == 0:
-                length += 1
-            if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                length += 1
-            if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                length += 1
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    length += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    length += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    length += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    length += 1
 
         perimeter += length
     return perimeter

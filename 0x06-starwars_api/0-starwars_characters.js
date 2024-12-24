803cfe5ca8af789640c9faf1657c1e4bbec90594
Adeboga Abigail <abbyboga@gmail.com>
2024-12-24 13:58:54 -0800

    Starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,22 +4,22 @@ const req = require('request');
 const API_URL = "https://swapi-api.hbtn.io/api";
 
 if (process.argv.length > 2) {
-	req(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-		if (err) {
-			console.log(err);
-		}
-		const charUrl = JSON.parse(body).characters;
-		const charName = charUrl.map(
-			url => new Promise((resolve, reject) => {
-				req(url, (promiseError, __, charReqBody) => {
-					if (promiseError) {
-						reject(promiseError);
-					}
-					resolve(JSON.parse(charReqBody).name);
-				});
-			}));
-		Promise.all(charName)
-			.then(names => console.log(names.join('\n')))
-			.catch(allError => console.log(allError));
-	});
+  req(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const charUrl = JSON.parse(body).characters;
+  const charName = charUrl.map(
+    url => new Promise((resolve, reject) => {
+      req(url, (promiseError, __, charReqBody) => {
+        if (promiseError) {
+          reject(promiseError);
+        }
+        resolve(JSON.parse(charReqBody).name);
+      });
+    }));
+    Promise.all(charName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allError => console.log(allError));
+    });
 }

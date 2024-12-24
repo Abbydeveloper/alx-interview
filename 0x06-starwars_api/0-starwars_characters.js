db3b6fbdb9fb12ca3795a87edace26e399050cb5
Adeboga Abigail <abbyboga@gmail.com>
2024-12-24 14:02:59 -0800

    Starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 
 const req = require('request');
-const API_URL = "https://swapi-api.hbtn.io/api";
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
   req(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-  if (err) {
-    console.log(err);
-  }
-  const charUrl = JSON.parse(body).characters;
-  const charName = charUrl.map(
-    url => new Promise((resolve, reject) => {
-      req(url, (promiseError, __, charReqBody) => {
-        if (promiseError) {
-          reject(promiseError);
-        }
-        resolve(JSON.parse(charReqBody).name);
-      });
-    }));
+    if (err) {
+      console.log(err);
+    }
+    const charUrl = JSON.parse(body).characters;
+    const charName = charUrl.map(
+      url => new Promise((resolve, reject) => {
+        req(url, (promiseError, __, charReqBody) => {
+          if (promiseError) {
+            reject(promiseError);
+          }
+          resolve(JSON.parse(charReqBody).name);
+        });
+      }));
     Promise.all(charName)
       .then(names => console.log(names.join('\n')))
       .catch(allError => console.log(allError));

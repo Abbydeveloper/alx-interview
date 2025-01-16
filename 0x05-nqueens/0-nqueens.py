e88418d5b5e5b207177e899297b4784a354aee3d
Adeboga Abigail <bogadeji@gmail.com>
2025-01-16 14:28:00 +0100

    Fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -57,7 +57,7 @@ def main():
     elif not sys.argv[1].isdigit():
         print("N must be a number")
         exit(1)
-    elif int(sys.argv[1] < 4):
+    elif int(sys.argv[1]) < 4:
         print("N must be at least 4")
         exit(1)
 

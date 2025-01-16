bd106e2902c3af1ba53d415ab62ca07b8576c6da
Adeboga Abigail <abbyboga@gmail.com>
2025-01-16 19:55:35 +0000

    Fix code issues

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,6 +4,18 @@ Prime Game
 """
 
 
+def remove_prime_multiples(list_primes, i):
+    """
+        Remove multiples of prime numbers
+    """
+
+    for j in range(2, len(list_primes)):
+        try:
+            list_primes[j * i] = 0
+        except (ValueError, IndexError):
+            break
+
+
 def isWinner(x, nums):
     """
     Find the winner of x rounds of the Prime Game
@@ -14,17 +26,19 @@ def isWinner(x, nums):
 
     if (x <= 0 or nums is None or x != len(nums)):
         return None
-        
+    
     maria, ben = 0, 0
 
     choice = [1 for x in range(sorted(nums)[-1] + 1)]
     choice[0], choice[1] = 0, 0
 
     for a in range(2, len(choice)):
-        try:
-            choice[a * a] = 0
-        except (ValueError, IndexError):
-            break
+        remove_prime_multiples(choice, a)
+        #try:
+
+        #choice[a * a] = 0
+        #except (ValueError, IndexError):
+            #break
 
     for i in nums:
         if sum(choice[0:i + 1]) % 2 == 0:

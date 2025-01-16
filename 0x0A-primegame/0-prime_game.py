c664127498f2457684657f1a9262eea38b67209c
Adeboga Abigail <bogadeji@gmail.com>
2025-01-16 19:25:34 +0100

    Initialize prime game file

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Prime Game
+"""
+
+
+def isWinner(x, nums):
+  """
+  Find the winner of x rounds of the Prime Game
+
+  Maria and Ben are playing a game.
+  Determine who the winner of each game is
+  """
